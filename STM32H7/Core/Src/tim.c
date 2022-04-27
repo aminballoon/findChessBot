a5e4aa45a291b14b9ad848bf3592acefe19e7cd3
Krittamet Thawong <37761388+timor2542@users.noreply.github.com>
2022-04-27 13:00:22 +0700

    Change Joint State Timer to 20Hz

--- STM32H7/Core/Src/tim.c
+++ STM32H7/Core/Src/tim.c
@@ -623,9 +623,9 @@ void MX_TIM17_Init(void)
 
   /* USER CODE END TIM17_Init 1 */
   htim17.Instance = TIM17;
-  htim17.Init.Prescaler = 240-1;
+  htim17.Init.Prescaler = 2400-1;
   htim17.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim17.Init.Period = 20000-1;
+  htim17.Init.Period = 5000-1;
   htim17.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim17.Init.RepetitionCounter = 0;
   htim17.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
