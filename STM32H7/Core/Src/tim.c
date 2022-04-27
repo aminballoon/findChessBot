e6cc839178b50568401012686fbc1dd1a779592c
Krittamet Thawong <37761388+timor2542@users.noreply.github.com>
2022-04-27 13:31:37 +0700

    Edit Joint State Timer to 50Hz

--- STM32H7/Core/Src/tim.c
+++ STM32H7/Core/Src/tim.c
@@ -625,7 +625,7 @@ void MX_TIM17_Init(void)
   htim17.Instance = TIM17;
   htim17.Init.Prescaler = 2400-1;
   htim17.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim17.Init.Period = 5000-1;
+  htim17.Init.Period = 2000-1;
   htim17.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim17.Init.RepetitionCounter = 0;
   htim17.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
